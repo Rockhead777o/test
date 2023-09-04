bdb7757b94af4a6a37e23e80bde88c97812f6328
Rockhead777o <52448588+Rockhead777o@users.noreply.github.com>
2023-09-04 15:06:22 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,23 +1,23 @@
 terraform {
+  required_version = ">=1.3"
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "=0.1.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.2"
+      version = ">= 3.51, < 4.0"
     }
-  }
-}
-
-provider "azapi" {
-  default_location = "eastus"
-  default_tags = {
-    team = "Azure deployments"
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
+
+provider "random" {}
