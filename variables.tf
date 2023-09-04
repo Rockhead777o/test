26ba68afd8a96613898c184f3bebf9981f99bf5f
Rockhead777o <52448588+Rockhead777o@users.noreply.github.com>
2023-09-04 16:21:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "cluster_log_analytics_workspace_name" {
 
 variable "vnet_subnet_id" {
   type        = string
-  default     = "/subscriptions/81f39976-e79a-495d-9fe2-9605dee64f16/resourceGroups/LennarSREPoc/providers/Microsoft.Network/virtualNetworks/rk-aks-vnet/rk-aks-sn"
+  default     = "/subscriptions/81f39976-e79a-495d-9fe2-9605dee64f16/resourceGroups/LennarSREPoc/providers/Microsoft.Network/virtualNetworks/rk-aks-vnet/subnets/rk-aks-sn"
 }
 
 variable "cluster_name" {
