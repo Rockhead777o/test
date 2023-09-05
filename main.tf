a268df240d5d59c92f5438bea41a27e94777df62
Rockhead777o <52448588+Rockhead777o@users.noreply.github.com>
2023-09-05 12:32:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,5 +21,5 @@ module "aks" {
   agents_max_count                      =   3
   agents_min_count                      =   1
   rbac_aad_managed                      =   true
-  network_plugin                        =   "CNI"
+  network_plugin                        =   "azure"
 }
